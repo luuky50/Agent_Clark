7adb880fc824b313d0c6f4636a8955d01d9de8d5
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-02-08 11:08:07 +0100

    the basics of the damage system

---
+++ Assets/Scripts/Managers/RobotHealth.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotHealth : MonoBehaviour
+{
+    private float health;
+    private float damage;
+    public RobotHealth(float newHealth, float newDamage)
+    {
+        health = newHealth;
+        damage = newDamage;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            OutgoingDamage(damage);
+            Destroy(this.gameObject);
+        }
+    }
+
+    /// <summary>
+    /// handles ingoing damage
+    /// </summary>
+    /// <param name="damage"></param>
+    public void IngoingDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    /// <summary>
+    /// handles outgoing damage
+    /// </summary>
+    /// <param name="damage"></param>
+    public void OutgoingDamage(float damage)
+    {
+        DamageManager.instance.DamageToPlayer(damage);
+    }
+}
