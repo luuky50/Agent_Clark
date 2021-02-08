cf4aa974069fe76ef5f0cac1692bcc492f3bfb66
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-02-08 11:24:17 +0100

    implemented requested changes

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -6,7 +6,7 @@ public class PlayerHealth : MonoBehaviour
 {
     private float health;
     private float damage;
-
+    /// Contains information about this object's current health and the damage this object deals
     public PlayerHealth(float health, float damage)
     {
         this.health = health;
