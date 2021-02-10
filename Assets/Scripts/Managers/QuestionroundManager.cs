c7d89d0cf6d799b19854ce8f375cb90e26e7a392
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-02-10 14:27:11 +0100

    beginnings of questionrounds
    only open questions implemented. multiple choice still needs to be done

---
+++ Assets/Scripts/Managers/QuestionroundManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionroundManager : SingletonComponent<QuestionroundManager>
+{
+    Dictionary<string, string> Questions = new Dictionary<string, string>()
+    {
+        {"What year is it?\n" +
+            "Example: !answer 2014", "2021"}
+    };
+
+    GameObject QuestionRoundPanel;
+    
+    KeyValuePair<string, string> currentQuestion;
+    void Start()
+    {
+        InitializeQuestionRoundUI();
+    }
+
+    private void InitializeQuestionRoundUI()
+    {
+        QuestionRoundPanel = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            BeginQuestionRound();
+        }
+    }
+
+    public void BeginQuestionRound()
+    {
+        currentQuestion = Questions.ElementAt(Random.Range(0, Questions.Count - 1));
+        QuestionRoundPanel.GetComponentInChildren<Text>().text = currentQuestion.Key;
+    }
+
+    public void ValidateQuestion(string Answer)
+    {
+        if (currentQuestion.Value == Answer)
+        {
+            Debug.Log("Question Answerd Right!");
+        }
+        else
+        {
+            Debug.Log(currentQuestion.Value);
+            Debug.Log("Question Answerd Wrong!");
+        }
+    }
+}
--- Assets/Scripts/TwitchClient.cs
+++ Assets/Scripts/TwitchClient.cs
@@ -7,116 +7,123 @@ using UnityEngine;
 
 public class TwitchClient : MonoBehaviour
 {
-	[SerializeField] //[SerializeField] Allows the private field to show up in Unity's inspector. Way better than just making it public
-	private string _channelToConnectTo = Secrets.USERNAME_FROM_OAUTH_TOKEN;
+    [SerializeField] //[SerializeField] Allows the private field to show up in Unity's inspector. Way better than just making it public
+    private string _channelToConnectTo = Secrets.USERNAME_FROM_OAUTH_TOKEN;
 
-	public RobotMovement robotMovement;
+    public RobotMovement robotMovement;
 
-	private Client _client;
+    private Client _client;
 
-	private void Start()
-	{
-		// To keep the Unity application active in the background, you can enable "Run In Background" in the player settings:
-		// Unity Editor --> Edit --> Project Settings --> Player --> Resolution and Presentation --> Resolution --> Run In Background
-		// This option seems to be enabled by default in more recent versions of Unity. An aditional, less recommended option is to set it in code:
-		// Application.runInBackground = true;
-		robotMovement = GameObject.Find("Robot").gameObject.GetComponent<RobotMovement>();
+    private void Start()
+    {
+        // To keep the Unity application active in the background, you can enable "Run In Background" in the player settings:
+        // Unity Editor --> Edit --> Project Settings --> Player --> Resolution and Presentation --> Resolution --> Run In Background
+        // This option seems to be enabled by default in more recent versions of Unity. An aditional, less recommended option is to set it in code:
+        // Application.runInBackground = true;
+        robotMovement = GameObject.Find("Robot").gameObject.GetComponent<RobotMovement>();
 
-		//Create Credentials instance
-		ConnectionCredentials credentials = new ConnectionCredentials(Secrets.USERNAME_FROM_OAUTH_TOKEN, Secrets.OAUTH_TOKEN);
+        //Create Credentials instance
+        ConnectionCredentials credentials = new ConnectionCredentials(Secrets.USERNAME_FROM_OAUTH_TOKEN, Secrets.OAUTH_TOKEN);
 
-		// Create new instance of Chat Client
-		_client = new Client();
+        // Create new instance of Chat Client
+        _client = new Client();
 
-		// Initialize the client with the credentials instance, and setting a default channel to connect to.
-		_client.Initialize(credentials, _channelToConnectTo);
+        // Initialize the client with the credentials instance, and setting a default channel to connect to.
+        _client.Initialize(credentials, _channelToConnectTo);
 
-		// Bind callbacks to events
-		_client.OnConnected += OnConnected;
-		_client.OnConnectionError += OnFailedToConnect;
+        // Bind callbacks to events
+        _client.OnConnected += OnConnected;
+        _client.OnConnectionError += OnFailedToConnect;
         _client.OnError += _client_OnError;
-		_client.OnJoinedChannel += OnJoinedChannel;
-		_client.OnMessageReceived += OnMessageReceived;
-		_client.OnChatCommandReceived += OnChatCommandReceived;
+        _client.OnJoinedChannel += OnJoinedChannel;
+        _client.OnMessageReceived += OnMessageReceived;
+        _client.OnChatCommandReceived += OnChatCommandReceived;
         _client.OnWhisperReceived += _client_OnWhisperReceived;
 
-		// Connect
-		_client.Connect();
+        // Connect
+        _client.Connect();
 
-	}
+    }
 
-	private void _client_OnWhisperReceived(object sender, TwitchLib.Client.Events.OnWhisperReceivedArgs e)
-	{
-		Debug.Log("Message: " + e.WhisperMessage.Message);
-		
-		_client.SendWhisper(e.WhisperMessage.Username, "Its working!");
-	}
+    private void _client_OnWhisperReceived(object sender, TwitchLib.Client.Events.OnWhisperReceivedArgs e)
+    {
+        Debug.Log("Message: " + e.WhisperMessage.Message);
+
+        _client.SendWhisper(e.WhisperMessage.Username, "Its working!");
+    }
 
     private void _client_OnError(object sender, TwitchLib.Communication.Events.OnErrorEventArgs e)
     {
-		Debug.LogError($"The program didnt work correctly: {e.Exception.Message}");
+        Debug.LogError($"The program didnt work correctly: {e.Exception.Message}");
     }
 
     private void OnConnected(object sender, TwitchLib.Client.Events.OnConnectedArgs e)
-	{
-		Debug.Log($"The bot {e.BotUsername} succesfully connected to Twitch.");
+    {
+        Debug.Log($"The bot {e.BotUsername} succesfully connected to Twitch.");
 
-		if (!string.IsNullOrWhiteSpace(e.AutoJoinChannel))
-			Debug.Log($"The bot will now attempt to automatically join the channel provided when the Initialize method was called: {e.AutoJoinChannel}");
-	}
+        if (!string.IsNullOrWhiteSpace(e.AutoJoinChannel))
+            Debug.Log($"The bot will now attempt to automatically join the channel provided when the Initialize method was called: {e.AutoJoinChannel}");
+    }
     private void OnFailedToConnect(object sender, TwitchLib.Client.Events.OnConnectionErrorArgs e)
     {
-		Debug.Log($"The bot {e.BotUsername} failed to connect");
+        Debug.Log($"The bot {e.BotUsername} failed to connect");
     }
 
-	private void OnJoinedChannel(object sender, TwitchLib.Client.Events.OnJoinedChannelArgs e)
-	{
-		Debug.Log($"The bot {e.BotUsername} just joined the channel: {e.Channel}");
-		_client.SendMessage(e.Channel, "I just joined the channel! PogChamp");
-	}
+    private void OnJoinedChannel(object sender, TwitchLib.Client.Events.OnJoinedChannelArgs e)
+    {
+        Debug.Log($"The bot {e.BotUsername} just joined the channel: {e.Channel}");
+        _client.SendMessage(e.Channel, "I just joined the channel! PogChamp");
+    }
 
-	private void OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
-	{
-		Debug.Log($"Message received from {e.ChatMessage.UserId}: {e.ChatMessage.Message}");
-	}
-
-	
-	private void OnChatCommandReceived(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
-	{
-		switch (e.Command.CommandText)
-		{
-			case "d":
-				_client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot to the right");
-				robotMovement.MoveSideways(SidewaysDirections.right);
-				break;
-			case "a":
-				_client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot to the left");
-				robotMovement.MoveSideways(SidewaysDirections.left);
-				break;
-			case "w":
-				_client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot up");
-				robotMovement.MoveSideways(SidewaysDirections.up);
-				break;
-			case "s":
-				_client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot down");
-				robotMovement.MoveSideways(SidewaysDirections.down);
-				break;
-			default:
-				_client.SendMessage(e.Command.ChatMessage.Channel, $"Unknown chat command: {e.Command.CommandIdentifier}{e.Command.CommandText}");
-				break;
-		}
-	}
+    private void OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
+    {
+        if (e.ChatMessage.Message.Contains("!"))
+        {
+            if (e.ChatMessage.Message.Contains("!answer"))
+            {
+                QuestionroundManager.instance.ValidateQuestion(e.ChatMessage.Message.Substring(8));
+            }
+        }
+        //  Debug.Log($"Message received from {e.ChatMessage.UserId}: {e.ChatMessage.Message}");
+    }
 
 
-	//TODO: This will be removed in final version
+    private void OnChatCommandReceived(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
+    {
+        switch (e.Command.CommandText)
+        {
+            case "right":
+                _client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot to the right");
+                robotMovement.MoveSideways(SidewaysDirections.right);
+                break;
+            case "left":
+                _client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot to the left");
+                robotMovement.MoveSideways(SidewaysDirections.left);
+                break;
+            case "up":
+                _client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot up");
+                robotMovement.MoveSideways(SidewaysDirections.up);
+                break;
+            case "down":
+                _client.SendMessage(e.Command.ChatMessage.Channel, "You moved the twitch bot down");
+                robotMovement.MoveSideways(SidewaysDirections.down);
+                break;
+            default:
+
+                break;
+        }
+    }
+
+
+    //TODO: This will be removed in final version
     private void Update()
-	{
-		// Don't call the client send message on every Update,
-		// this is sample on how to call the client,
-		// not an example on how to code.
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			_client.SendMessage(_channelToConnectTo, "I pressed the space key within Unity.");
-		}
-	}
+    {
+        // Don't call the client send message on every Update,
+        // this is sample on how to call the client,
+        // not an example on how to code.
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _client.SendMessage(_channelToConnectTo, "I pressed the space key within Unity.");
+        }
+    }
 }
