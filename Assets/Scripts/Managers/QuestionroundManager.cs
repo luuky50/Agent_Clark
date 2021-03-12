2b7b26c1d7636d53630bd9b83b12525ff44d6874
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-03-12 10:27:07 +0100

    Revert "Revert "GUI updATES""
    This reverts commit 180594cd5865ef30caea6dae55f31796aa456b06.

--- Assets/Scripts/Managers/QuestionroundManager.cs
+++ Assets/Scripts/Managers/QuestionroundManager.cs
@@ -18,12 +18,33 @@ public class QuestionroundManager : SingletonComponent<QuestionroundManager>
     private void Start()
     {
         // TODO: initialize this only when we are in a scene where this is relevant
+<<<<<<< Updated upstream
+=======
+        isMultipleChoice = false;
+>>>>>>> Stashed changes
         InitializeQuestionRoundUI();
     }
 
     private void InitializeQuestionRoundUI()
     {
+<<<<<<< Updated upstream
         QuestionRoundPanel = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+=======
+        if (isMultipleChoice)
+        {
+            QuestionRoundPanel = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+            QuestionRoundPanel.transform.GetChild(0).gameObject.SetActive(true);
+            QuestionRoundPanel.transform.GetChild(1).gameObject.SetActive(true);
+            QuestionRoundPanel.transform.GetChild(2).gameObject.SetActive(false);
+        }
+        else
+        {
+            QuestionRoundPanel = GameObject.Find("Canvas").transform.GetChild(0).gameObject;
+            QuestionRoundPanel.transform.GetChild(0).gameObject.SetActive(false);
+            QuestionRoundPanel.transform.GetChild(1).gameObject.SetActive(false);
+            QuestionRoundPanel.transform.GetChild(2).gameObject.SetActive(true);
+        }
+>>>>>>> Stashed changes
     }
 
     private void Update()
@@ -40,8 +61,90 @@ public class QuestionroundManager : SingletonComponent<QuestionroundManager>
     /// </summary>
     public void BeginQuestionRound()
     {
+<<<<<<< Updated upstream
         currentQuestion = Questions.ElementAt(Random.Range(0, Questions.Count - 1));
         QuestionRoundPanel.GetComponentInChildren<Text>().text = currentQuestion.Key;
+=======
+        if (isMultipleChoice)
+        {
+            InitializeAnswersOfTeamsList();
+            correctAnswer = Random.Range(0, 3);
+            currentMultipleChoiceQuestion = multipleChoiceQuestions[Random.Range(0, multipleChoiceQuestions.Count - 1)];
+            QuestionRoundPanel.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<Text>().text = currentMultipleChoiceQuestion.Question;
+            var answers = QuestionRoundPanel.transform.GetChild(0).GetComponentsInChildren<Text>();
+
+            int goodQuestionPosition = Random.Range(0, 4);
+            answers[goodQuestionPosition].text = currentMultipleChoiceQuestion.Answer;
+
+            List<string> wrongAnswers = new List<string>() {
+                {currentMultipleChoiceQuestion.WrongAnswerOne },
+                {currentMultipleChoiceQuestion.WrongAnswerTwo },
+                {currentMultipleChoiceQuestion.WrongAnswerThree }
+            };
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (goodQuestionPosition != i)
+                {
+                    answers[i].text = wrongAnswers[Random.Range(0,3)];
+                }
+            }
+
+        }
+        else
+        {
+            currentQuestion = OpenQuestions.ElementAt(Random.Range(0, OpenQuestions.Count - 1));
+            QuestionRoundPanel.GetComponentInChildren<Text>().text = currentQuestion.Key;
+        }
+    }
+
+
+    /// <summary>
+    /// validates question, checks if it is good and adds it to the givin answers list
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="participantID"></param>
+    public void ValidateClosedQuestion(string message, int participantID)
+    {
+        if (message == currentMultipleChoiceQuestion.Answer)
+        {
+            foreach (Participant p in TeamManager.instance.Participants)
+            {
+                if (p.ParticipantID == participantID)
+                {
+                    Debug.Log(p.team + "   and good");
+                    if (!PlayerAnswersOfMultipleChoice[p.team].ContainsKey(participantID))
+                    {
+                        PlayerAnswersOfMultipleChoice[p.team].Add(participantID, true);
+                    }
+                    else
+                    {
+                        //TODO: message to sender containing that he already voted
+                        Debug.Log("Already voted");
+                    }
+                    setUIPanel();
+                }
+            }
+        }
+        else
+        {
+            foreach (Participant p in TeamManager.instance.Participants)
+            {
+                if (p.ParticipantID == participantID)
+                {
+                    Debug.Log(p.team + "   and wrong");
+                    if (!PlayerAnswersOfMultipleChoice[p.team].ContainsKey(participantID))
+                    {
+                        PlayerAnswersOfMultipleChoice[p.team].Add(participantID, false);
+                    }
+                    else
+                    {
+                        //TODO: message to sender containing that he already voted
+                    }
+                    setUIPanel();
+                }
+            }
+        }
+>>>>>>> Stashed changes
     }
 
     /// <summary>
